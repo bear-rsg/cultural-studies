074d877c2a56c9b4257f28156bd9dd0012b93fca
Mike Allaway <m.j.allaway@bham.ac.uk>
2023-05-17 11:02:30 +0100

    added list filters to admin

--- django/researchdata/admin.py
+++ django/researchdata/admin.py
@@ -244,6 +244,7 @@ class EntityAdminView(GenericAdminView):
     Customise the Entity section of the admin dashboard
     """
     list_display = ('name', 'use_as_template')
+    list_filter = ('type',)
     inlines = (
         EntityHistoryInline,
         RelEntityAndEntityInline,
@@ -275,6 +276,7 @@ class EventAdminView(GenericAdminView):
     Customise the Event section of the admin dashboard
     """
     list_display = ('name', 'location', 'start', 'end', 'use_as_template')
+    list_filter = ('type', 'language', 'activity')
     inlines = (
         RelEntityAndEventInline,
         RelEventAndItemInline,
@@ -328,6 +330,7 @@ class ItemAdminView(GenericAdminView):
     Customise the Item section of the Django admin
     """
     list_display = ('name', 'item_id', 'finding_aid', 'is_a_collective_item', 'publication_status', 'use_as_template')
+    list_filter = ('is_a_collective_item', 'publication_status', 'type', 'language', 'media')
     inlines = (
         RelEntityAndItemInline,
         RelEventAndItemInline,
@@ -383,9 +386,11 @@ class PersonAdminView(GenericAdminView):
     list_display = ('first_name',
                     'last_name',
                     'other_names',
+                    'title',
                     'is_a_group_of_persons',
                     'group_of_persons_description',
                     'use_as_template')
+    list_filter = ('is_a_group_of_persons', 'title')
     inlines = (
         PersonHistoryInline,
         RelEntityAndPersonInline,
